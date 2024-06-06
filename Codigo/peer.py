681afe15d9d95abbec7907bd4b589be230437030
claradasneves <anadasnevesbarreto@gmailcom>
2024-06-06 17:06:59 -0300

    aaaaaaa

--- Codigo/peer.py
+++ Codigo/peer.py
@@ -2,6 +2,7 @@ import threading
 import pickle
 import socket
 import time
+from mensagem import Message
 
 class Peer:
     # Inicia cada um dos nós - OK
@@ -11,6 +12,7 @@ class Peer:
         self.vizinhos = []
         self.chave_valor = {}  # Armazenamento de pares chave-valor
         self.mensagens_vistas = set() # Para armazenar mensagens já vistas
+        self.seq_no = 0
 
         # Inicializa o servidor em uma thread separada
         self.server_thread = threading.Thread(target=self.inicia_servidor)
@@ -25,16 +27,16 @@ class Peer:
         servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         servidor.bind((self.endereco, self.porta))
         servidor.listen(5) # 5 é o número máximo de conexões pendentes que o sistema permitirá antes de recusar novas conexões
-        print(f'Servidor criado: {self.endereco}:{self.porta}')
+        print(f'Servidor criado: {self.endereco}:{self.porta}\n')
         
         # Mantém o servidor em execução contínua, permitindo que ele aceite conexões de clientes indefinidamente
         while True:
             peer_socket, addr = servidor.accept() # Bloqueia a execução até que uma nova conexão seja aceita
             print(f'Conexão de {addr}')
-            threading.Thread(target=self.handle_client, args=(peer_socket,)).start() # Cria nova thread para cada conexão de cliente
+            threading.Thread(target=self.handle_peer, args=(peer_socket,)).start() # Cria nova thread para cada conexão de cliente
 
-    # Lida com as mensagens recebidas de um cliente conectado (quando um servidor aceita uma conexão ou quando nó adiciona um vizinho) - OK
-    def handle_client(self, peer_socket):
+    # Lida com as mensagens recebidas de um peer conectado (quando um servidor aceita uma conexão ou quando nó adiciona um vizinho) - OK
+    def handle_peer(self, peer_socket):
         while True:
             try:
                 mensagem = peer_socket.recv(4096) # Recebe a mensagem
@@ -53,65 +55,54 @@ class Peer:
 
     # Peer se conecta a outro peer na rede
     def conecta_peer(self, endereco, porta):
+        print('teste')
         try:
             peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            peer_socket.connect((endereco, porta)) # Isso aqui nao tá funcionando no meu pc MORTEEEEEE
+            peer_socket.connect((endereco, porta))
             return peer_socket
-        except Exception as e:
-            print(f'Erro ao conectar o peer {endereco}:{porta}: {e}')
+        except:
+            print(f'Erro ao conectar!')
             return None
-
-    """
-            # Enviar a mensagem "hello"
-            mensagem = "hello"
-            peer_socket.sendall(mensagem.encode())
-            
-            # Receber a resposta do peer
-            resposta = peer_socket.recv(1024).decode()
-            
-            if resposta == "hello":
-                self.peers.append(peer_socket)
-                threading.Thread(target=self.handle_peer, args=(peer_socket,)).start()
-                print(f"Mensagem recebida: {endereco_peer}:{porta_peer} HELLO")
-                print(f"Adicionando vizinho na tabela: {self.endereco}:{self.porta}")
-            else:
-                peer_socket.close()
-                print(f"Conexão com {endereco_peer}:{porta_peer} recusada, resposta não foi 'hello'.")
-        except Exception as e:
-            print(f'Error connecting to peer {endereco}:{porta}: {e}')
-            return None
-
-        # print(f'Connected to peer {endereco_peer}:{porta_peer}')
-        # TODO: adicionar mensagem "HELLO" para envio -> confirmação
-
-    """
 
     def adiciona_vizinho(self, endereco, porta):
         # Verificar se o vizinho já está na lista de vizinhos
         for vizinho_socket in self.vizinhos:
             if vizinho_socket.getpeername() == (endereco, porta):
-                print(f'Vizinho {endereco}:{porta} já está na lista de vizinhos')
+                print(f'Vizinho {endereco}:{porta} já está na tabela de vizinhos')
                 return
+        mensagem = self.formata_mensagem('HELLO')
+        self.log_encaminhamento(mensagem, endereco, porta)
+
         try:
             vizinho_socket = self.conecta_peer(endereco, porta)
-            resposta = self.handle_hello(vizinho_socket)
-
-            if resposta == "hello":
-                self.vizinhos.append(vizinho_socket)
-                threading.Thread(target=self.handle_client, args=(vizinho_socket,)).start()
-                print(f'Conexão estabelecida com vizinho {endereco}:{porta}')
-            else:
-                print(f'Falha na conexão com vizinho {endereco}:{porta}, resposta não foi "hello"')
-                vizinho_socket.close()
-            # except Exception as e:
-                print(f'Erro ao conectar com vizinho {endereco}:{porta}')
-        except Exception as e:
-            print(f"Erro ao adicionar vizinho {endereco}:{porta}: {e}")
+            if vizinho_socket:
+                self.seq_no += 1
+                # se pa da pra substituir isso aqui por algum handle
+                resposta = self.handle_request('HELLO', vizinho_socket)
+                # self.envia_mensagem(vizinho_socket, mensagem)
+                # resposta = vizinho_socket.recv(4096)
+                # resposta = pickle.loads(resposta)
+                if resposta == "hello":
+                    self.vizinhos.append(vizinho_socket)
+                    threading.Thread(target=self.handle_peer, args=(vizinho_socket,)).start()
+                    print(f'Conexão estabelecida com vizinho {endereco}:{porta}')
+                else:
+                    vizinho_socket.close()
+        except:
+            print(f'    Erro ao conectar!')
             return None
-  
+        
+    def formata_mensagem(self, operacao, *argumentos):
+        mensagem = f"{self.endereco}:{self.porta} {self.seq_no} 1 {operacao}"
+        if argumentos:
+            mensagem += " " + " ".join(map(str, argumentos))
+        return mensagem + "\n"
+
+    def log_encaminhamento(self, mensagem, endereco, porta):
+        print(f'Encaminhando mensagem "{mensagem.strip()}" para {endereco}:{porta}')
 
     # Carrega os vizinhos a partir de um arquivo txt
-    def load_neighbors(self, peer, filename):
+    def load_neighbors(self, filename): # tinha um 'peer' aqui, tirei... não faz sentido
         with open(filename, 'r') as file:
             for line in file:
                 endereco_vizinho, porta_vizinho = line.strip().split(':')
@@ -131,43 +122,15 @@ class Peer:
                 chave, valor = line.strip().split(' ')
                 peer.armazena_valor(chave, valor)
 
-    # Verificar o STORE e ver se tá passando a mensagem de forma certinha :)
+    # Verificar o STORE e ver se tá passando a mensagem de forma certinha
     def handle_request(self, request, peer_socket):
-        operacao = request.get('type')
+        operacao = request.get('operacao')
 
         if operacao == 'HELLO':
-            self.handle_hello(peer_socket)
+            resposta = self.handle_hello(peer_socket)
+            return resposta
         elif operacao == 'SEARCH':
             self.handle_search(request, peer_socket)
-        #elif operacao == "STORE":
-        # Não tenho certeza se essa STORE precisa
-            # self.handle_store(request, peer_socket)
-            """
-            chave = request['chave']
-            metodo = request['metodo']
-            origem = request['origem']
-            ttl = request['ttl']
-            seq_no = request['seq_no']
-            if metodo == 'FLOODING':
-                visitados = request['visitados']
-                flooding(self, chave, origem, ttl, seq_no, visitados)
-            elif metodo == 'RANDOM_WALK':
-                ultimo_vizinho = request['ultimo_vizinho']
-                random_walk(self, chave, origem, ttl, seq_no, ultimo_vizinho)
-            elif metodo == 'DFS':
-                visitados = request['visitados']
-                busca_em_profundidade(self, chave, origem, ttl, seq_no,)
-        # Ver se vai usar esse FOUND e se precisa de algum outro
-        elif operacao == 'FOUND':
-            chave = request['chave']
-            valor = request['valor']
-            origem = request['origem']
-            if (self.endereco, self.porta) == origem:
-                print(f'Found data: {chave} -> {valor}')
-            else:
-                self.envia_mensagem_para_peers(request)
-            
-            """
     
     def handle_hello(self, peer_socket):
         try:
@@ -177,8 +140,8 @@ class Peer:
             print(f'Enviou HELLO para {self.endereco}:{self.porta}')
 
             return resposta
-        except Exception as e:
-            print(f'Erro ao enviar HELLO para {self.endereco}:{self.porta}: {e}')
+        except:
+            print(f'Erro ao enviar HELLO para {self.endereco}:{self.porta}')
 
     def handle_search(self, request, peer_socket):
         from Grafo.buscas import flooding, random_walk, busca_em_profundidade
@@ -220,29 +183,25 @@ class Peer:
             'seq_no': seq_no
             }
             busca_em_profundidade(mensagem, peer_socket)
-        """
-        elif ttl > 0:
+        elif ttl > 0: # Decrementa o TTL e verifica se ainda é válido
             ttl -= 1
             hop_count += 1
             mensagem = {
-                'origin': origin,
-                'seqno': seqno,
+                'origem': origem,
+                'seqno': self.seq_no,
                 'ttl': ttl,
-                'search': 'SEARCH',
-                'mode': mode,
                 'last_hop_port': self.porta,
-                'key': key,
+                'chave': chave,
                 'hop_count': hop_count
             }
             self.encaminha_mensagem(mensagem, peer_socket)
-        """
 
     # Envia mensagem para determinado peer
     def envia_mensagem (self, peer_socket, mensagem):
         try:
             peer_socket.send(pickle.dumps(mensagem))
-        except Exception as e:
-            print(f'Erro ao enviar mensagem para o peer: {e}')
+        except:
+            print(f'Erro ao enviar mensagem para o peer!')
 
     # Transmite a mensagem para os outros nós, exceto o atual
     def transmite_mensagem(self, mensagem, exclude_socket=None):
@@ -251,5 +210,5 @@ class Peer:
             if vizinho != exclude_socket:
                 try:
                     self.envia_mensagem(vizinho, mensagem)
-                except Exception as e:
-                    print(f'Error forwarding message to neighbor: {e}')
+                except:
+                    print(f'Erro ao transmitir mensagens para os vizinhos!')
